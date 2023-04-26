1319a5d88e619f43143788b5e5fa7dfc6f6fe9bf
Raphzer-brr <r.lechappe@alpes.net>
2023-04-26 20:58:39 +0200

    Probleme du timout ...

--- playbooks/ProCurve/HP_ProCurve_SSH_public_key.py
+++ playbooks/ProCurve/HP_ProCurve_SSH_public_key.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
+from datetime import datetime
+from colorama import Fore, Back, Style
+import sys
 import netmiko
 import os
-from datetime import datetime
+
 
 def get_public_key():
     with open(os.path.expanduser("~/.ssh/id_rsa.pub")) as f:
@@ -20,26 +23,32 @@ def configure_hp_procurve(hpProcurvesLocal):
         "aaa authentication ssh enable public-key"
         #"crypto key generate ssh rsa bits 3072", Il faudrait pouvoir regenéré la clef ssh avec le max de bit possible si cela n'a pas été fait comme ca mais cette commande risque de faire platé le programme
     ]
+    
     try:
         with netmiko.ConnectHandler(**hpProcurvesLocal) as connection:
             start_time = datetime.now()
-            print(f"Prompt de départ : {connection.find_prompt()}")
             output = connection.send_config_set(commands)
             try:
                 output += connection.send_config_set(f"ip ssh public-key manager '{public_key}'")
             except netmiko.exceptions.ReadTimeout:
-                print(f"""La connexion a expiré en raison d'une attente trop longue, \nvérifie que la clef public est bien dans le switch
-
-    Commande de vérification : 
-                show crypto client-public-key
-                ssh -i /path/to/myprivatekey {hpProcurvesLocal["ip"]}@{hpProcurvesLocal["ip"]} 
-                """)
+                timeout = True
+                print(f"{Back.BLUE}Déploiment réussit !{Style.RESET_ALL} : Petit soucis de timeout mais rien de grave\n")
+                print(f"{Fore.BLUE}Commande de vérification : {Style.RESET_ALL}")
+                print("show crypto client-public-key")
+                print(f"ssh -i /path/to/myprivatekey {hpProcurvesLocal['ip']}@{hpProcurvesLocal['ip']}\n")
             end_time = datetime.now()
-        print(output)
-        print(f"Exec time for 1 device: {end_time - start_time}")
+        if (not timeout):
+            print(f"{Back.BLUE}Déploiment réussit !{Style.RESET_ALL} : Tout est OK")
+        print(f"{Fore.BLUE}Commande effectué : {Style.RESET_ALL}{output}")
+        print(f"{Back.MAGENTA}Benchmark{Style.RESET_ALL}")
+        print(f"{Fore.MAGENTA}{'Exec time for 1 device : ':<25}{Style.RESET_ALL}{end_time - start_time}")
+
     except netmiko.exceptions.NetmikoAuthenticationException as e:
-        err = str(e).replace("Authentication to device failed.\n", "").replace("Common causes of this problem are:\n", "")
-        print(f"Message personnel de debug : \n0. L'authentification par clef publique est déjà déployer\n\nMessage de Netmiko: {err}")
+        err = str(e).replace("Authentication to device failed.\n", "").replace("Common causes of this problem are:\n", "").replace("\n\n\nAuthentication failed.", "").replace("\nDevice settings: hp_procurve 192.168.1.22:22", "")
+        print(f"{Back.RED}ERREUR{Style.RESET_ALL}: Authentification Fail")
+        print(f"{Fore.RED}Message personnel de debug :{Style.RESET_ALL}\n0. L'authentification par clef publique est déjà déployer\n")
+        print(f"{Fore.RED}Message de Netmiko:{Style.RESET_ALL} {err}\n")
+        sys.exit(1)
 def main():
     hpProcurves = {
         "ip":'192.168.1.22',
@@ -52,7 +61,7 @@ def main():
     start_time_tot = datetime.now()
     configure_hp_procurve(hpProcurves)
     end_time_tot = datetime.now()
-    print(f"Exec time tot: {end_time_tot - start_time_tot}")
+    print(f"{Fore.MAGENTA}{'Exec time tot : ':<25}{Style.RESET_ALL}{end_time_tot - start_time_tot}")
 
 if __name__ == "__main__":
     main()
